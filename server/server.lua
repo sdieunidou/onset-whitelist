0c3eb5bbcfde935c93678caafda0d9d5d4c16c57
Kotus <15613323+Kotus-s@users.noreply.github.com>
2019-11-17 20:36:58 +0100

    :rotating_light: Remove linter warning

--- server/server.lua
+++ server/server.lua
@@ -1,24 +1,21 @@
-local log = require('packages/whitelist/util/log')
-local whitelist = require('packages/whitelist/server/whitelist')
+local log = require( 'packages/whitelist/util/log' )
+local whitelist = require( 'packages/whitelist/server/whitelist' )
 
 function OnPackageStart()
-     log.info("whitelist by mTxServ.com!")
-     whitelist.reload();
+     log.info( 'whitelist by mTxServ.com!' )
+     whitelist.reload()
 end
-AddEvent("OnPackageStart", OnPackageStart)
+AddEvent( 'OnPackageStart', OnPackageStart )
 
-function OnPlayerSteamAuth(player)
-    local steamId = tostring(GetPlayerSteamId(player))
-    if not whitelist.isAuthorized(steamId)  then
-            KickPlayer("You are not allowed to join this server!")
+function OnPlayerSteamAuth( player )
+    local steamId = tostring( GetPlayerSteamId( player ) )
+    if ( not whitelist.isAuthorized( steamId ) ) then
+            KickPlayer( player, 'You are not allowed to join this server!' )
     end
 end
-AddEvent("OnPlayerSteamAuth", OnPlayerSteamAuth)
+AddEvent( 'OnPlayerSteamAuth', OnPlayerSteamAuth )
 
-AddCommand("whitelist-reload", function(player)
-    if not IsPlayerAdmin(player) then
-        return
-    end
-    
+AddCommand( 'whitelist-reload', function( player )
+    if ( not IsPlayerAdmin( player ) ) then return end
     whitelist.reload()
 end)
