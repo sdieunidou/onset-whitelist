0d6132176c2cc2b028dbaf502753de171ab6142c
Kotus <bastien35@gmail.com>
2019-11-17 21:03:41 +0100

    :rotating_light: Remove linter warning

--- server/server.lua
+++ server/server.lua
@@ -1,21 +1,25 @@
-local log = require( 'packages/whitelist/util/log' )
-local whitelist = require( 'packages/whitelist/server/whitelist' )
+local package_name = GetPackageName()
+local log = require( ( 'packages/%s/util/log' ):format( package_name ) )
+local whitelist = require( ( 'packages/%s/server/whitelist' ):format( package_name ) )
 
 function OnPackageStart()
-     log.info( 'whitelist by mTxServ.com!' )
-     whitelist.reload()
+	 log.info( 'whitelist by mTxServ.com!' )
+	 local is_realod = whitelist.reload()
 end
 AddEvent( 'OnPackageStart', OnPackageStart )
 
 function OnPlayerSteamAuth( player )
-    local steamId = tostring( GetPlayerSteamId( player ) )
-    if ( not whitelist.isAuthorized( steamId ) ) then
-            KickPlayer( player, 'You are not allowed to join this server!' )
-    end
+	local steam_id = tostring( GetPlayerSteamId( player ) )
+	if ( not whitelist.isAuthorized( steam_id ) ) then
+			KickPlayer( player, 'You are not allowed to join this server!' )
+	end
 end
 AddEvent( 'OnPlayerSteamAuth', OnPlayerSteamAuth )
 
 AddCommand( 'whitelist-reload', function( player )
-    if ( not IsPlayerAdmin( player ) ) then return end
-    whitelist.reload()
+	if ( not IsPlayerAdmin( player ) ) then return end
+	whitelist.reload()
 end)
+
+AddFunctionExport( 'isWhitelisted', whitelist.isAuthorized )
+AddFunctionExport( 'reloadWhitelist', whitelist.reload )
--- server/whitelist.lua
+++ server/whitelist.lua
@@ -1,38 +1,39 @@
-local log = require('packages/'..GetPackageName()..'/util/log')
+local package_name = GetPackageName()
+local log = require( ( 'packages/%s/util/log' ):format( package_name ) )
 local open = io.open
+local whitelist_path = ( 'packages/%s/whitelist.json' ):format( package_name )
 
 local whitelist = {}
-whitelist.data= {}
+whitelist.data = {}
 
-local function read_whitelist_file(path)
-    local file = open(path, "rb")
-    if not file then return nil end
-    local content = file:read "*a"
-    file:close()
-    return content
+local function read_whitelist_file( path )
+	local file = open( path, 'rb')
+	if ( not file ) then return nil end
+	local content = file:read( '*a' )
+	file:close()
+	return content
 end
 
 function whitelist.reload()
-  local content = read_whitelist_file('packages/'..GetPackageName()..'/whitelist.json');
-  
-  if content == nil then
-      log.info('whitelist reloaded but there is not player allowed to join the server! Edit packages/'..GetPackageName()..'/whitelist.json !')
-      return
-  end
-  
-  local data = json_decode(content)
+	local content = read_whitelist_file( whitelist_path )
 
-   for k,v in pairs(data) do
-      whitelist.data[v] = v
-   end
+	if ( content == nil ) then
+		log.info( ( 'Whitelist reloaded but there is not player allowed to join the server! Edit %s !' ):format( whitelist_path ) )
+		return false
+	end
+
+	local data = json_decode( content )
+	if ( data == nil ) then return false end
+
+	for i = 1, #data do
+		whitelist.data[ data[ i ] ] = true
+	end
+
+	return true
 end
 
-function whitelist.isAuthorized(steamId)
-    if whitelist.data[steamId] == nil then
-        return false
-    end
-  
-    return true
+function whitelist.isAuthorized( steam_id )
+	return whitelist.data[ steam_id ]
 end
 
 return whitelist
